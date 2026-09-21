287295632d2d522998412f966eb6d587e2984432
Shrimp <441956394@qq.com>
2026-09-21 17:43:37 +0800

    add change to "hello.cpp"

--- demo/hello.cpp
+++ demo/hello.cpp
@@ -5,5 +5,6 @@ int main(){
     cout << "helloworld" << endl;
     cout << "this is from main branch" << endl;
     cout << "this is from develop branch" << endl;
+    cout << "this is new from develop branch" << endl;
     return 0;
 }
